4277da6d5c18d4075fbb9f91bec1e56d58a800bf
Thanpisit Banyam <porsohani@gmail.com>
2026-03-25 18:37:42 +0700

    declare parameter pos, step, speed and triangle lenght to prepare formula of triangle

--- triangle.py
+++ triangle.py
@@ -11,3 +11,24 @@ from underautomation.yaskawa.high_speed_e_server.position_command_type import Po
 from underautomation.yaskawa.high_speed_e_server.position_command_classification import PositionCommandClassification
 from underautomation.yaskawa.high_speed_e_server.position_command_operation_coordinate import PositionCommandOperationCoordinate
 
+def main():
+    # =========================================================
+    # ⚙️ ส่วนตั้งค่าการทำงาน 
+    # =========================================================
+    
+    ROBOT_SPEED = 10         
+    STEP_SIZE =  80    
+    DELAY_BETWEEN_STEPS = 1 
+    
+    fixed_x = 299.859
+    home_y = 0.000
+    home_z = 128.600
+    
+    fixed_rx = 180.000
+    fixed_ry = -8.976
+    fixed_rz = 0.000
+    
+    L = 150.0 
+    h = L * (math.sqrt(3) / 2) 
+    
+    
